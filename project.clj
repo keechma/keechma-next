f7948447fa12759dc5a8a0f96985571d8d1e7163
Mihael Konjevic <konjevic@gmail.com>
2021-07-01 14:34:32 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/next "0.1.2"
+(defproject keechma/next "0.1.3"
   :description "Data driven state management library for single page apps"
   :url "https://github.com/keechma/keechma-next"
   :license {:name "MIT License"
